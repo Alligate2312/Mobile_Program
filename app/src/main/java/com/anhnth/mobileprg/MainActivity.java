2de28efe517830f70ad9c01dcf468c2d61369efb
anh95488@gmail.com <anh95488@gmail.com>
2025-07-15 17:53:02 +0700

    update activities

--- app/src/main/java/com/anhnth/mobileprg/MainActivity.java
+++ app/src/main/java/com/anhnth/mobileprg/MainActivity.java
@@ -1,24 +1,20 @@
-package com.anhnth.mobileprg;
+package com.example.prm_swd;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Button loginButton = findViewById(R.id.btnLogin);
+        Button registerButton = findViewById(R.id.btnRegister);
+
+        loginButton.setOnClickListener(v -> startActivity(new Intent(this, LoginActivity.class)));
+        registerButton.setOnClickListener(v -> startActivity(new Intent(this, RegisterActivity.class)));
     }
 }
